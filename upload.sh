d33fb23284203af7686e78f924f1028ed4a390b2
Scott Chi <MiracleM4n@gmail.com>
2014-03-24 13:49:36 -0400

    Create upload.sh for DBO uploading.

---
+++ upload.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+file=$(find -L . -name "*.jar");
+
+if echo "$ul" | grep -v -q "SNAPSHOT"; then
+    curl -X POST -F "commit=$1" -F "type=$2" -F "key=$BUKKIT_API_KEY" -F "file=@$file" http://pc-serv.ca/ci_upload.php;
+fi
