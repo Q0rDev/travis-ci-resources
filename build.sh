9b3d329e8f30dd9d9fc95fefc816f794bf11c851
Scott Chi <MiracleM4n@gmail.com>
2014-03-25 18:49:07 -0400

    Change buildNumber to commit.

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 # Check if commit is not a pull request, if git repo is official, and if branch is master. If checks pass, deploy artifacts to Maven repository.
 if [[ $TRAVIS_PULL_REQUEST = "false" ]] && [[ $TRAVIS_REPO_SLUG = "Q0rDev/"* ]] && [[ $TRAVIS_BRANCH = "master" ]]; then
-  mvn deploy -DbuildNumber=b${TRAVIS_BUILD_NUMBER}trvs --settings target/travis/settings.xml
+  mvn deploy -Dcommit=${TRAVIS_COMMIT:0:7}git --settings target/travis/settings.xml
 # If above check fails, just package.
 else
-  mvn package -DbuildNumber=b${TRAVIS_BUILD_NUMBER}trvs
+  mvn package -Dcommit=${TRAVIS_COMMIT:0:7}git
 fi
