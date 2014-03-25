061f7e154b03e932cf3df73130365316d027c0d3
Scott Chi <MiracleM4n@gmail.com>
2014-03-25 19:49:16 -0400

    Remove git portion of commit var.

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 # Check if commit is not a pull request, if git repo is official, and if branch is master. If checks pass, deploy artifacts to Maven repository.
 if [[ $TRAVIS_PULL_REQUEST = "false" ]] && [[ $TRAVIS_REPO_SLUG = "Q0rDev/"* ]] && [[ $TRAVIS_BRANCH = "master" ]]; then
-  mvn deploy -Dcommit=${TRAVIS_COMMIT:0:7}git --settings target/travis/settings.xml
+  mvn deploy -Dcommit=${TRAVIS_COMMIT:0:7} --settings target/travis/settings.xml
 # If above check fails, just package.
 else
-  mvn package -Dcommit=${TRAVIS_COMMIT:0:7}git
+  mvn package -Dcommit=${TRAVIS_COMMIT:0:7}
 fi
