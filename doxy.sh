6dcc2518694029f801fb1834ddd6fd2f30b2f701
Scott Chi <MiracleM4n@gmail.com>
2014-04-08 14:57:23 -0400

    Fix doxy.sh

--- doxy.sh
+++ doxy.sh
@@ -3,9 +3,11 @@
 # Only to be used with MChat
 if [[ $TRAVIS_REPO_SLUG = "Q0rDev/MChat" ]]; then
   doxygen ${TRAVIS_BUILD_DIR}/target/travis/DoxyFile & wait
-
+  
+  COMMIT_MESSAGE=$(curl -X POST -F "commit=$TRAVIS_COMMIT" http://q0r.ca/ci/commit.php) & wait
+  
   cd ${TRAVIS_BUILD_DIR}/target/doxy/
   git add --all
-  git commit -m ${TRAVIS_COMMIT_MESSAGE}
+  git commit -m ${COMMIT_MESSAGE}
   git push https://Q0r-JD:${GH_PASSWORD}@github.com/Q0rDev/q0rdev-javadocs
 fi
