71e799919437a89f938c63c9176da24b3173961a
Scott Chi <MiracleM4n@gmail.com>
2014-04-09 00:22:43 -0400

    Directory fixes and more silencing of doxy.sh.

--- doxy.sh
+++ doxy.sh
@@ -3,11 +3,11 @@
 # Only to be used with MChat
 if [[ $TRAVIS_REPO_SLUG = "Q0rDev/MChat" ]]; then
   sudo apt-get install -qq doxygen > /dev/null & wait
-  doxygen ${TRAVIS_BUILD_DIR}/target/travis/DoxyFile & wait
+  doxygen target/travis/DoxyFile & wait
   
-  COMMIT_MESSAGE=$(curl -X POST -F "commit=$TRAVIS_COMMIT" http://q0r.ca/ci/commit.php) & wait
+  COMMIT_MESSAGE=$(curl -sS -X POST -F "commit=$TRAVIS_COMMIT" http://q0r.ca/ci/commit.php) & wait
   
-  cd ${TRAVIS_BUILD_DIR}/target/doxy/
+  cd target/doxy/
   git add --all
   git commit -m "${COMMIT_MESSAGE}"
   git push https://Q0r-JD:${GH_PASSWORD}@github.com/Q0rDev/q0rdev-javadocs
