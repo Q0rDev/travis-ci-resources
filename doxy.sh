d3d1ff096e1fef61d92d454c783335fa2d28f495
Scott Chi <MiracleM4n@gmail.com>
2014-04-09 00:03:58 -0400

    Add "apt-get install doxygen" to doxy.sh.

--- doxy.sh
+++ doxy.sh
@@ -2,12 +2,13 @@
 
 # Only to be used with MChat
 if [[ $TRAVIS_REPO_SLUG = "Q0rDev/MChat" ]]; then
+  sudo apt-get install doxygen & wait
   doxygen ${TRAVIS_BUILD_DIR}/target/travis/DoxyFile & wait
   
   COMMIT_MESSAGE=$(curl -X POST -F "commit=$TRAVIS_COMMIT" http://q0r.ca/ci/commit.php) & wait
   
   cd ${TRAVIS_BUILD_DIR}/target/doxy/
   git add --all
-  git commit -m ${COMMIT_MESSAGE}
+  git commit -m "${COMMIT_MESSAGE}"
   git push https://Q0r-JD:${GH_PASSWORD}@github.com/Q0rDev/q0rdev-javadocs
 fi
