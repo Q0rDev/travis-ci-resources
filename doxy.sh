81c0da4fbbebb0963b9acd56d03618270a07d7c6
Scott Chi <MiracleM4n@gmail.com>
2014-04-09 03:19:55 -0400

    Should fix all remaining bugs.

--- doxy.sh
+++ doxy.sh
@@ -2,12 +2,20 @@
 
 # Only to be used with MChat
 if [[ $TRAVIS_REPO_SLUG = "Q0rDev/MChat" ]]; then
-  sudo apt-get install -qq doxygen > /dev/null & wait
-  doxygen ${TRAVIS_BUILD_DIR}/target/travis/Doxyfile & wait
+  wget -q -P target http://ftp.stack.nl/pub/users/dimitri/doxygen-1.8.6.linux.bin.tar.gz & wait
+  tar -xf target/doxygen-*.tar.gz -C target
+  chmod +x target/doxygen-*/bin/doxygen
+  
+  ./target/doxygen-*/bin/doxygen ${TRAVIS_BUILD_DIR}/target/travis/Doxyfile & wait
   
   COMMIT_MESSAGE=$(curl -sS -X POST -F "commit=$TRAVIS_COMMIT" http://q0r.ca/ci/commit.php) & wait
   
   cd ${TRAVIS_BUILD_DIR}/target/doxy/
+  
+  git config --global user.email "MiracleM4n@gmail.com"
+  git config --global user.name "MiracleM4n"
+  git config --global push.default simple
+  
   git add --all
   git commit -m "${COMMIT_MESSAGE}"
   git push https://Q0r-JD:${GH_PASSWORD}@github.com/Q0rDev/q0rdev-javadocs
