72aeef74738f0340ca927c7532f51a4a79d812ac
Scott Chi <MiracleM4n@gmail.com>
2014-04-09 00:25:19 -0400

    Revert directory fixes. Fix Doxyfile naming.

--- doxy.sh
+++ doxy.sh
@@ -3,11 +3,11 @@
 # Only to be used with MChat
 if [[ $TRAVIS_REPO_SLUG = "Q0rDev/MChat" ]]; then
   sudo apt-get install -qq doxygen > /dev/null & wait
-  doxygen target/travis/DoxyFile & wait
+  doxygen ${TRAVIS_BUILD_DIR}/target/travis/Doxyfile & wait
   
   COMMIT_MESSAGE=$(curl -sS -X POST -F "commit=$TRAVIS_COMMIT" http://q0r.ca/ci/commit.php) & wait
   
-  cd target/doxy/
+  cd ${TRAVIS_BUILD_DIR}/target/doxy/
   git add --all
   git commit -m "${COMMIT_MESSAGE}"
   git push https://Q0r-JD:${GH_PASSWORD}@github.com/Q0rDev/q0rdev-javadocs
