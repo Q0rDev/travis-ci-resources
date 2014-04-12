30e62d72bda39cb8dc5d5e8c71895a8da033b244
Scott Chi <MiracleM4n@gmail.com>
2014-04-12 02:54:49 -0400

    Add forgotten checks to upload.sh.

--- upload.sh
+++ upload.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
-file=$(find -L . -name "*.jar");
+# Check if commit is not a pull request, if git repo is official, and if branch is master. If checks pass, upload artifacts to Bukkit.
+if [[ $TRAVIS_PULL_REQUEST = "false" ]] && [[ $TRAVIS_REPO_SLUG = "Q0rDev/"* ]] && [[ $TRAVIS_BRANCH = "master" ]]; then
+    # Find file to upload.
+    file=$(find -L . -name "*.jar");
 
-if echo "$file" | grep -v -q "SNAPSHOT"; then
-    if echo "$file" | grep -v -q "BETA"; then
-        type="r"
-    else
-        type="b"
-    fi
+    # Check to make sure File isn't a Snapshot.
+    if echo "$file" | grep -v -q "SNAPSHOT"; then
+        # Set Type to Beta or Release.
+        if echo "$file" | grep -v -q "BETA"; then
+            type="r"
+        else
+            type="b"
+        fi
     
-    curl -X POST -F "api_key=$BUKKIT_API_KEY" -F "commit=${TRAVIS_COMMIT:0:7}" -F "file=@$file" -F "file_type=$type" -F "slug=$TRAVIS_REPO_SLUG" http://q0r.ca/ci/upload.php;
+        curl -X POST -F "api_key=$BUKKIT_API_KEY" -F "commit=${TRAVIS_COMMIT:0:7}" -F "file=@$file" -F "file_type=$type" -F "slug=$TRAVIS_REPO_SLUG" http://q0r.ca/ci/upload.php;
+    fi
 fi
