bab7360590c488261d6932778b7f12d63e100aeb
Scott Chi <MiracleM4n@gmail.com>
2014-04-26 12:35:50 -0400

    Make building process a little quieter.

--- build.sh
+++ build.sh
@@ -2,8 +2,16 @@
 
 # Check if commit is not a pull request, if git repo is official, and if branch is master. If checks pass, deploy artifacts to Maven repository.
 if [[ $TRAVIS_PULL_REQUEST = "false" ]] && [[ $TRAVIS_REPO_SLUG = "Q0rDev/"* ]] && [[ $TRAVIS_BRANCH = "master" ]]; then
-  mvn deploy -Dcommit=${TRAVIS_COMMIT:0:7} --settings target/travis/settings.xml
+  mvn deploy -Dmaven.test.skip=true -Dcommit=${TRAVIS_COMMIT:0:7} --settings target/travis/settings.xml > build.txt
+  grep -v "Download" build.txt
+  
+  grep -q "BUILD SUCCESS" build.txt
+  exit $?;
 # If above check fails, just package.
 else
-  mvn package -Dcommit=${TRAVIS_COMMIT:0:7}
+  mvn package -Dmaven.test.skip=true -Dcommit=${TRAVIS_COMMIT:0:7} > build.txt
+  grep -v "Download" build.txt
+  
+  grep -q "BUILD SUCCESS" build.txt
+  exit $?;
 fi
