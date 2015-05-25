5b60dd6ce22d1466c717e3709734d856b5aae621
Scott Chi <miraclem4n@gmail.com>
2015-05-25 11:50:21 -0400

    Update doxy.sh to only create/upload from MChat-Sponge.

--- doxy.sh
+++ doxy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Check if commit is not a pull request, if git repo is MChat, and if branch is master. If checks pass, create and upload Doxygen files.
-if [[ $TRAVIS_PULL_REQUEST = "false" ]] && [[ $TRAVIS_REPO_SLUG = "Q0rDev/MChat" ]] && [[ $TRAVIS_BRANCH = "master" ]]; then
+if [[ $TRAVIS_PULL_REQUEST = "false" ]] && [[ $TRAVIS_REPO_SLUG = "Q0rDev/MChat-Sponge" ]] && [[ $TRAVIS_BRANCH = "master" ]]; then
   # Doxygen: Get / Extract / Set Executable.
   wget -q -P target http://ftp.stack.nl/pub/users/dimitri/doxygen-1.8.6.linux.bin.tar.gz & wait
   tar -xf target/doxygen-*.tar.gz -C target
